1d14e536188f5054ada95370a083798417dcb659
zancas <zancas@protonmail.com>
2020-01-18 17:32:26 +0000

    tweak names fix horrid orange color

--- plot_getop_latencies.py
+++ plot_getop_latencies.py
@@ -8,7 +8,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-class PathExtractor:
+class PathExtracts:
     def __init__(self, log_name):
         self.logpath = Path(log_name).absolute()
         self._extract_names()
@@ -19,8 +19,9 @@ class PathExtractor:
         self.core_name = self.log_base.rstrip(".log")
         self.log_data_ancestor =  self.logpath.parent.parent.parent 
 
-class PathSetter:
+class PathResults:
     def __init__(self, pathextractor):
+        self.pathextractor = pathextractor
         self._construct_json_data_path(pathextractor)
 
     def _construct_json_data_path(self, pe):
@@ -28,14 +29,15 @@ class PathSetter:
         self.data_path = self.data_dirpath / Path( pe.core_name + ".json")
 
 
-def plot(latencies):
-    xs = range(len(latencies))
-    ys = latencies
+def _set_plot_labels_title():
     plt.title("z_getoperationresult 'success' to z_listunspent latency")
     plt.ylabel('seconds')
     plt.xlabel('iteration')
-    plt.plot(xs, ys, '.')
-    plt.savefig("20_async_all_latencies.png")
+
+def plot_datavector(latencies, pathextractor, symbol):
+    xs = range(len(latencies))
+    ys = latencies
+    plt.plot(xs, ys, symbol)
 
 def extract_latencies(logpath):
     lines = open(logpath).readlines()
@@ -45,13 +47,18 @@ def extract_latencies(logpath):
     return [D(x) for x in lagtimes]
 
 def main():
-    path_extractor = PathExtractor(sys.argv[1])
-    latencies = extract_latencies(path_extractor.logpath)
-    print(latencies)
-    #latencies = json.loads(path_extractor.logpath)
-    #paths = PathSetter(path_extractor)
-    #pp(paths.data_path)
-
+    _set_plot_labels_title()
+    path_extractor1 = PathExtracts(sys.argv[1])
+    latencies1 = extract_latencies(path_extractor1.logpath)
+    plot_datavector(latencies1, path_extractor1, '.')
+    if len(sys.argv) == 3:
+        path_extractor2 = PathExtracts(sys.argv[2])
+        latencies1 = extract_latencies(path_extractor2.logpath)
+        plot_datavector(latencies1, path_extractor2, 'rx')
+        outputbasename = path_extractor1.core_name + '_v_' + path_extractor2.core_name + '.png'
+        plt.savefig( Path("./pngs/" ) / Path( outputbasename ) )
+    else:
+        plt.savefig( Path("./pngs/" ) / Path(pathextractor1.core_name + ".png") )
 
 if __name__ == '__main__':
     main()
